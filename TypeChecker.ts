f4e72649aff8912642431dc63ea0018361ac96ea
Rémy Jeancolas <remy.jeancolas@illogeek.eu>
2016-10-23 18:47:09 -0400

    First working version

--- TypeChecker.ts
+++ TypeChecker.ts
@@ -1,15 +1,56 @@
 import 'reflect-metadata';
 
 const propertiesToCheck = Symbol('propertiesToCheck');
+const paramsToCheck = Symbol('paramsToCheck');
 
-export function TypeCheckProperty(params: PropertyCheckParams = {}): any {
+export interface PropertyCheckParams {
+    type?: any;
+    arrayType?: any;
+    required?: boolean;
+    nullable?: boolean;
+}
+
+class ValidationError extends Error {
+    public fields: string[] = [];
+}
+
+export function TypesCheck(target: any, propertyKey: string, descriptor: TypedPropertyDescriptor<any>): any {
+    const originalMethod = descriptor.value;
+
+    descriptor.value = function(...args: any[]): any {
+        if (Array.isArray(target[paramsToCheck]) && target[paramsToCheck].hasOwnProperty(propertyKey)) {
+            for (let i = 0; i < args.length; i += 1) {
+                if (typeof target[paramsToCheck][propertyKey][i] !== 'undefined') {
+                    validate(args[i], target[paramsToCheck][propertyKey][i]);
+                }
+            }
+        }
+
+        return originalMethod.apply(this, args);
+    };
+}
+
+export function TypeCheck(type: any): any {
+    return (target: any, propertyKey: string | symbol, parameterIndex: number): any => {
+        if (!Array.isArray(target[paramsToCheck])) {
+            target[paramsToCheck] = [];
+        }
+        if (!target[paramsToCheck].hasOwnProperty(propertyKey)) {
+            target[paramsToCheck][propertyKey] = [];
+        }
+        target[paramsToCheck][propertyKey][parameterIndex] = type;
+    };
+}
+
+export function PropertyCheck(params: PropertyCheckParams = {}): any {
     return (target: any, key: string | symbol): any => {
         // Define property type
         const type = params.type ? params.type : Reflect.getMetadata("design:type", target, key);
 
         // Check if type is valid
+        let expectedType: any;
         try {
-            new type();
+            expectedType = new type();
         } catch (e) {
             // Type is invalid, stop here
             return;
@@ -23,45 +64,84 @@ export function TypeCheckProperty(params: PropertyCheckParams = {}): any {
         params.type = type;
         params.required = (typeof params.required === 'boolean') ? params.required : true;
         params.nullable = (typeof params.nullable === 'boolean') ? params.nullable : false;
+
+        // If type is array, check array type if provided
+        if (expectedType.constructor.name === 'Array' && params.arrayType) {
+            try {
+                expectedType = new params.arrayType(); 
+            } catch (e) {
+                delete params.arrayType;
+            }
+        } else {
+            delete params.arrayType;
+        }
+
         target[propertiesToCheck][key] = params;
     };
 }
 
-
-
-export interface PropertyCheckParams {
-    type?: any;
-    required?: boolean;
-    nullable?: boolean;
+export function validate(input: any, expectedType: any): void {
+    try {
+        validateInput(input, expectedType);
+    } catch (e) {
+        throw new Error(`${(<ValidationError> e).fields.join(' -> ')}: ${e.message}`);
+    }
 }
 
-export function validate(input: any, expectedType: any): void {
-    if (expectedType) {
-        const a = expectedType;
-        // Try to instantiate the exepcted type to see if it's valid 
-        try {
-            expectedType = new expectedType();
-        } catch (e) {
-            return;
-        }
+function validateInput(input: any, expectedType: any, arrayType: any = null): void {
+    // Try to instantiate the expected type to see if it's valid 
+    try {
+        expectedType = new expectedType();
+    } catch (e) {
+        return;
+    }
 
-        // If type has propertiesToCheck, it's a complex type
-        if (expectedType[propertiesToCheck]) {
-            console.log(expectedType.constructor.name, expectedType[propertiesToCheck], JSON.stringify(arguments[0]));
-            Object.keys(expectedType[propertiesToCheck]).forEach(key => {
-                const checkParams: PropertyCheckParams = expectedType[propertiesToCheck][key];
-                // Validate required
-                if (checkParams.required && !input.hasOwnProperty(key)) {
-                    throw new Error(`[${expectedType.constructor.name}] Missing required field: ${key}`);
+    // If type has propertiesToCheck, it's a complex type with fields to validate
+    if (expectedType[propertiesToCheck]) {
+        Object.keys(expectedType[propertiesToCheck]).forEach(key => {
+            const checkParams: PropertyCheckParams = expectedType[propertiesToCheck][key];
+            // Validate required
+            if (input && input.hasOwnProperty(key)) {
+                if (!checkParams.nullable && input[key] == null) {
+                    const error = new ValidationError('Field can\'t be null');
+                    error.fields.push(key);
+                    throw error;
                 }
+            } else if (checkParams.required) {
+                const error = new ValidationError('Field is required');
+                error.fields.push(key);
+                throw error;
+            }
 
-                // Validate properties recursively
-                if (input[key]) {
-                    validate(input[key], checkParams.type);
+            // Validate properties recursively
+            if (input[key]) {
+                try {
+                    validateInput(input[key], checkParams.type, checkParams.arrayType);
+                } catch (e) {
+                    (<ValidationError> e).fields.unshift(key);
+                    throw e;
                 }
-            });
-        } else { // Else it's a basic type
-            console.log(expectedType.constructor.name);
-        }
+            }
+        });
+    } else { // Else it's a basic type or a complex type with no validation
+        const constructorName = expectedType.constructor.name;
+        if (constructorName !== 'Object') {
+            const providedType = typeof input;
+            if (constructorName === 'Array') {
+                if (!Array.isArray(input)) {
+                    throw new ValidationError(`Expecting array, received ${providedType} ${JSON.stringify(input)}`);
+                }
+                if (arrayType) {
+                    input.forEach(item => {
+                        validateInput(item, arrayType);
+                    });
+                }
+            } else {
+                expectedType = typeof expectedType.valueOf();
+                if (providedType !== expectedType) {
+                    throw new ValidationError(`Expecting ${expectedType}, received ${providedType} ${JSON.stringify(input)}`);
+                }
+            }
+        }            
     }
 }
--- index.ts
+++ index.ts
@@ -1,24 +1,37 @@
-import {TypeCheckProperty, validate} from './TypeChecker';
+import {TypesCheck, TypeCheck, PropertyCheck, validate} from './TypeChecker';
 
 interface FooBar {
     description: string;
 }
 
 class Bar {
-    @TypeCheckProperty({required: true})
-    public description: string;
-    public lastName: string;
+    @PropertyCheck({required: true, arrayType: String})
+    public description: string[];
+    @PropertyCheck()
+    public count: Number;
+    @PropertyCheck({nullable: false})
+    public baz: Function;
 }
 
 class Foo {
-    @TypeCheckProperty({required: false})
+    @PropertyCheck({required: true, nullable: true})
     public name: string;
-    @TypeCheckProperty()
-    public bar: Bar;
+    @PropertyCheck({arrayType: Bar})
+    public bar: Bar[];
 }
 
 const foo = new Foo();
-const a: any = {description: 'aaa'};
-foo.bar = a;//new Bar();
-foo.bar.lastName = 'foobar';
-validate(foo, Foo);
+foo.name = null;
+const a: any = {description: ['5'], count: 3, baz: () => { console.log('foo');}};
+foo.bar = [a];//new Bar();
+// foo.bar.count = 'aaa';
+
+class aaa {
+    @TypesCheck
+    public aaa(a: number, @TypeCheck(Foo) foo: Foo): void {
+        console.log(a);
+    }
+}
+let b = new aaa();
+b.aaa(Math.round(Math.random() * 100), foo);
+// validate(foo, Foo);
