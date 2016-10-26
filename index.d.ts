e41f543600fdae1cd56854a867efb9be5d9788d8
Rémy Jeancolas <remy.jeancolas@ssense.com>
2016-10-26 11:13:15 -0400

    Fix typings definitions

--- index.d.ts
+++ index.d.ts
@@ -9,5 +9,5 @@ export class ValidationError extends Error {}
 
 export function TypesCheck(target: any, propertyKey: string, descriptor: TypedPropertyDescriptor<any>): any;
 export function TypeCheck(type: any): any;
-export function PropertyCheck(params: PropertyCheckParams): any;
+export function PropertyCheck(params?: PropertyCheckParams): any;
 export function validate(input: any, expectedType: any, arrayType?: any): void;
